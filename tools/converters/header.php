8f758971e3b16ee9cbc958a847a5e7f91b5da8bb
Adéla Goldová <adela.goldova@gmail.com>
2013-09-09 20:52:24 +0200

    Added converters. MD5, SHA1, Base64, Hex and binary. All expect MD5 and SHA1 works both ways

---
+++ tools/converters/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <style>
+        body {margin-top:50px;}
+    </style>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="../../assets/js/html5shiv.js"></script>
+    <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="navbar navbar-fixed-top">
+        <div class="navbar navbar-inner">
+            <a href="index.php" class="brand">Converters</a>
+        </div>
+    </div>
