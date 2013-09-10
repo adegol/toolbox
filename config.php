d878b0e232957766b1987dc65028db4c90ae50cd
Adéla Goldová <adela.goldova@gmail.com>
2013-09-10 02:37:41 +0200

    First version ready

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
 define('DB_DSN', 'mysql:dbname=toolbox;host=localhost');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'adminad');
