29e13e980ea303098f935151287b1dbd4de233f4
Adéla Goldová <adela.goldova@gmail.com>
2013-09-11 01:29:34 +0200

    Added tool list with links

--- index.php
+++ index.php
@@ -2,7 +2,36 @@
 <html lang="en">
 <head>
     <title>Toolbox</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <style>
+        /*body {margin-top:50px;}
+        textarea {height:550px;}*/
+    </style>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="../../assets/js/html5shiv.js"></script>
+    <script src="../../assets/js/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <fieldset>
+                <legend>Table of Content</legend>
+                <ul>
+                    <li><a href="tools/converters/index.php">Converters</a></li>
+                    <li><a href="tools/pastebin/index.php">Pastebin</a></li>
+                </ul>
+            </fieldset>
+        </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
